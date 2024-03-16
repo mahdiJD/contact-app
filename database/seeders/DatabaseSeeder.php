40f21f969a6a937ddddb7e15c628576f600c4b5b
“[Mahdi <[bijarimahdi@gmail.com]>
2024-03-16 16:06:55 +0330

    make fack data in DB

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Company;
 use App\Models\Contact;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,7 +16,17 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        Company::factory(10)->hasContact(10)->create();
+//        Company::factory(10)->hasContact(10)->create();
+
+        User::factory(10)->has(
+            Company::factory(10)->has(
+                Contact::factory(10)->state(function ($attributes,Company $company){
+                    return [
+                        'user_id' => $company->user_id,
+                    ];
+                })
+            )
+        )->create();
 
 //        Company::factory()->count(10)->create();
 //        Contact::factory()->count(100)->create();
